2be86f383b4524b454a316ddac779e89f28dd63a
Eric De Carufel <edecarufel@pyxis-tech.com>
2016-08-29 21:20:51 -0400

    Initial commit

---
+++ src/net/decarufel/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package net.decarufel.helloworld;
+
+import static java.lang.System.out;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        out.println("Hello World!");
+    }
+}
