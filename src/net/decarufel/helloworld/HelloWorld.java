6b40a5994e911fead402ffe85dd8235e1af9881d
Eric De Carufel <edecarufel@pyxis-tech.com>
2016-08-29 23:02:41 -0400

    Fix

--- src/net/decarufel/helloworld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package net.decarufel.helloworld;
-
-import static java.lang.System.out;
-
-public class HelloWorld {
-    public static void main(String[] args){
-        out.println("Hello World!");
-    }
-}
